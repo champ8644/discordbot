f73b09cb397d431d8efa63362b8571e472b6d4bc
Champ <varis_champ@hotmail.com>
2020-10-03 01:56:08 +0700

    save

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Discord = require("discord.js");
+const pjson = require("./package.json");
 const bot = new Discord.Client();
 
 bot.on("ready", () => {
-  console.log(`${bot.user.username} Is Online!`);
+  console.log(`${bot.user.username} ver${pjson.version} Is Online!`);
 });
 
-bot.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
-  }
-});
+// bot.on("message", (msg) => {
+//   if (msg.content === "ping") {
+//     msg.reply("Pong!");
+//   }
+// });
 
 bot.login(process.env.token);
