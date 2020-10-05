ae30db952b62c07008c6108531274048b466c056
Champ <varis_champ@hotmail.com>
2020-10-05 22:39:22 +0700

    update status bot

--- onReady/welcomeScreen.js
+++ onReady/welcomeScreen.js
@@ -1,7 +1,11 @@
 const pjson = require("../package.json");
 const { bot } = require("../utils/Discord");
+const { getChannel } = require("../utils/getChannel");
 
 function welcomeScreen() {
   console.log(`${bot.user.username} ver${pjson.version} Is Online!`);
+  getChannel("ห้องสถานะบอท").send(
+    `${bot.user.username} ver${pjson.version} Is Online!`
+  );
 }
 module.exports = { welcomeScreen };
