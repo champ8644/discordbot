cf870031add480d91700660e9f4452728031b063
Champ <varis_champ@hotmail.com>
2020-11-24 10:47:09 +0700

    save

--- onReady/HBDCheck.js
+++ onReady/HBDCheck.js
@@ -1,22 +1,41 @@
 const { db } = require("../utils/postgres.js");
-const { subDays } = require("date-fns");
+const { addHours, isSameDay, format } = require("date-fns");
+
+// function isSameDayInJapan(leftDate, rightDate) {
+//   return isSameDay(addHours(leftDate, 2), addHours(rightDate, 2));
+// }
+
+function getDateStr(date) {
+  return format(date, "dd/MM");
+}
+
+function getDateInJapan(date) {
+  return addHours(date, 2);
+}
 
 async function updateLastLogin(date) {
-  const text = "UPDATE meta SET lastlogin=($1)";
-  const values = [date];
-  return db.query(text, values);
+  const text = "UPDATE meta SET lasthbd = ($1)";
+  const values = [getDateStr(date)];
+  const res = await db.query(text, values);
+  return res?.rowCount;
 }
 
 async function getLastLogin() {
-  const text = "SELECT lastlogin from meta";
+  const text = "SELECT lasthbd from meta";
   const res = await db.query(text);
   const { rows } = res;
-  const { lastlogin } = rows[0];
-  return lastlogin;
+  const lasthbd = rows && rows[0]?.lasthbd;
+  return lasthbd;
 }
 
 async function HBDCheck() {
-  const get = await getLastLogin();
+  //   await db.query("INSERT INTO meta(lastlogin) VALUES($1)", [new Date()]);
+  const getLastInJapan = await getLastLogin();
+  const nowInJapan = getDateInJapan(new Date());
+  if (!isSameDay(getLastInJapan, nowInJapan)) {
+    await sendHBD();
+    await updateLastLogin(nowInJapan);
+  }
 }
 
 module.exports = { HBDCheck };
